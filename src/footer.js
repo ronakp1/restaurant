f1c12d1869964778e8d0a752fbdbb2da9b8aa2c0
Ronak Patel <ronakpatel@live.co.uk>
2021-04-29 16:52:09 +0100

    changes

--- src/footer.js
+++ src/footer.js
@@ -6,7 +6,7 @@ const createFooter = () => {
     const footerBar = createOwnElement('div', 'footerBar', null);
     contentDiv.appendChild(footerBar);
     const madeByParagraph = createOwnElement('p', null, null);
-    madeByParagraph.innerHTML = "Made by Ronak Patel. Github: X";
+    madeByParagraph.innerHTML = "Made by Ronak Patel.";
     footerBar.appendChild(madeByParagraph);
 
 }
